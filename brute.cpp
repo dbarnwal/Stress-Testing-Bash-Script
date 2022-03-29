ed8a9ed9c8cd2f1b3572aeb7c8cb960ea5f6d4db
Deepak Barnwal <deepakbarnwal_19367@aitpune.edu.in>
2022-03-29 16:30:32 +0530

    Brute Force Approach

---
+++ brute.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define  FastIO ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define int long long
+signed main(){
+    FastIO;
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "wb", stdout);
+    #endif
+    return 0;
+}
